438ab98ce9e0468993e144b5411849445c47059d
Viktoria <125218715+Viktoria-kramar@users.noreply.github.com>
2023-07-05 00:25:35 +0500

    conftest.py

--- 25/conftest.py
+++ 25/conftest.py
@@ -12,7 +12,6 @@ pytest.driver = webdriver.Chrome()
 def testing():
     pytest.driver = webdriver.Chrome()
     pytest.driver.maximize_window()
-    # Переходим на страницу авторизации
     pytest.driver.get("http://petfriends.skillfactory.ru/login")
 
     yield
@@ -22,15 +21,10 @@ def testing():
 
 @pytest.fixture()
 def go_to_pets_page():
-    # неявное ожидание 5 сек при каждом шаге
     pytest.driver.implicitly_wait(5)
-    # Вводим email
     pytest.driver.find_element(By.ID, "email").send_keys(valid_email)
-    # Вводим пароль
     pytest.driver.find_element(By.ID, "pass").send_keys(valid_password)
-    # Нажимаем на кнопку входа в аккаунт
     pytest.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
-    # Переходим на страницу со списком питомцев пользователя
     pytest.driver.find_element(By.XPATH, '//a[@href="/my_pets"]').click()
-    # явное ожидание 10 сек.
     WebDriverWait(pytest.driver, 10).until(ec.presence_of_element_located((By.ID, "all_my_pets")))
+    
